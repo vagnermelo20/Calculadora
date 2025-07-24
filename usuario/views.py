5fc201f478557fefdaa76dabe81917bdd6e4aad0
Vagner Montenegro de Melo <vagnermontenegro2@gmail.com>
2025-07-24 13:24:21 -0300

    ajustao as view do usuario para se padronizar com os models

--- usuario/views.py
+++ usuario/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth import login, logout
+from .models import Usuario
 
-# Create your views here.
+class CadastroView(View):
+    def get(self, request):
+        return render(request, 'cadastro.html')
+
+    def post(self, request):
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        senha_confirmacao = request.POST.get('senha_confirmacao')
+
+        if senha != senha_confirmacao:
+            context = {'error': 'As senhas não coincidem!'}
+            return render(request, 'cadastro.html', context)
+        
+        if Usuario.objects.filter(email=email).exists():
+            context = {'error': 'Este email já está em uso.'}
+            return render(request, 'cadastro.html', context)
+
+        user = Usuario(nome=nome, email=email)
+        user.salvar_senha_hasheada(senha)
+        user.save()
+        return redirect('login')
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        
+        try:
+            user = Usuario.objects.get(email=email)
+            if user.checar_senha_hasheada(senha):
+                login(request, user)
+                return redirect('pagina_principal')
+            else:
+                context = {'error': 'Email ou senha inválidos.'}
+                return render(request, 'login.html', context)
+        except Usuario.DoesNotExist:
+            context = {'error': 'Email ou senha inválidos.'}
+            return render(request, 'login.html', context)
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
