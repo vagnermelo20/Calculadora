95179edafff1b7831232cd4fbbf0aa0cdb0f632a
Vagner Montenegro de Melo <vagnermontenegro2@gmail.com>
2025-07-24 17:52:33 -0300

    Criada a função ready no apps do usuario para evitar o update_last_login
    função criada para evitar o uso do atributo last_login do Django, bloqueando o update_last_login, que é o utilizado como padrão na função login do Django para atualizar o último login do usuário.
    Pois nós não temos o last_login no models Usuario.

--- usuario/apps.py
+++ usuario/apps.py
@@ -1,6 +1,18 @@
+# usuario/apps.py
 from django.apps import AppConfig
-
 
 class UsuarioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'usuario'
+
+
+    '''função criada para evitar o uso da função last_login do Django, bloqueando o update_last_login,
+       que é o utilizado como padrão na função login do Django.'''
+    def ready(self):
+        from django.contrib.auth.signals import user_logged_in
+        from django.contrib.auth.models import update_last_login
+
+        user_logged_in.disconnect(
+            update_last_login,
+            dispatch_uid="update_last_login"
+        )
