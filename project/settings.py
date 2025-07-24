535ae1665472db24f8129eafd03604512837e03c
Vagner Montenegro de Melo <vagnermontenegro2@gmail.com>
2025-07-24 17:35:24 -0300

    adicionei um custom backend

--- project/settings.py
+++ project/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'usuario',
-    'calculadora',
+    'usuario.apps.UsuarioConfig',
+    'calculadora.apps.CalculadoraConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'usuario.backends.CustomAuthBackend',          # seu backend custom
+    'django.contrib.auth.backends.ModelBackend',  # fallback padrão
+]
