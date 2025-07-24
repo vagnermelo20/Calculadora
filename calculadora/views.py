1991f2f40a207a016ec122ece55faf59fd70f370
Vagner Montenegro de Melo <vagnermontenegro2@gmail.com>
2025-07-24 17:22:10 -0300

    views inicial da calculadora

--- calculadora/views.py
+++ calculadora/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class CalculadoraView(LoginRequiredMixin, View):
+    login_url = '/login/'
+
+    def get(self, request):
+        return render(request, 'calculadora/calculadora.html')
