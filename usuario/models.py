a1f7c9d1dc66629d8bb699196937915db222ad69
Vagner Montenegro de Melo <vagnermontenegro2@gmail.com>
2025-07-24 13:17:13 -0300

    consertei a nomenclatura dos models de usuario
    Coloquei o nome dos atributos em minusculo, nas estou enviando em maiusculo para o banco de dados

--- usuario/models.py
+++ usuario/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
-'''Eu sei que o padrão do nome dos objetos são minusculos, mas eu coloquei maiusculo para seguir o padrão que me foi mandado de tabela'''
+'''nome padrao dos objetos em minusculo, mas coloquei maiusculo para enviar na tabela'''
 
 class Usuario(models.Model):
-    IDUsuario = models.AutoField(primary_key=True)
-    Nome = models.CharField(max_length=255, null=False, blank=False)
-    Email = models.EmailField(max_length=255, unique=True, null=False, blank=False)
-    Senha = models.CharField(max_length=128, null=False, blank=False)
-    DtInclusao = models.DateTimeField(auto_now_add=True)
+    id_usuario  = models.AutoField(primary_key=True, db_column='IDUsuario')
+    nome = models.CharField(max_length=255, null=False, blank=False, db_column='Nome')
+    email = models.EmailField(max_length=255, unique=True, null=False, blank=False, db_column='Email')
+    senha = models.CharField(max_length=128, null=False, blank=False, db_column='Senha')
+    dt_inclusao = models.DateTimeField(auto_now_add=True, db_column='DtInclusao')
 
     '''Metodos para conseguir hashear e checar a senha hasheada'''
     def salvar_senha_hasheada(self, senha_nao_hash):
@@ -17,7 +17,7 @@ class Usuario(models.Model):
         return check_password(senha_nao_hash, self.Senha)
 
     def __str__(self):
-        return f"{self.Nome} ({self.Email})"
+        return f"{self.nome} ({self.email})"
 
     class Meta:
         # Formatar o nome de Usuario na tabela
